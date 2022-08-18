2340501f264ce26bfebf0f7a0475c9098bd5de1a
Rico <hochiw@oursky.com>
2022-08-18 14:48:18 +0800

    Ignore insertion of the same collection to db
    refs #12

--- pkg/mutator/nft_collection.go
+++ pkg/mutator/nft_collection.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 
-	"github.com/authgear/authgear-nft-indexer/pkg/model"
 	"github.com/authgear/authgear-nft-indexer/pkg/model/eth"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/extra/bunbig"
@@ -15,18 +14,26 @@ type NFTCollectionMutator struct {
 	Session *bun.DB
 }
 
-func (q *NFTCollectionMutator) InsertNFTCollection(blockchainNetwork model.BlockchainNetwork, contractName string, contractAddress string) (*eth.NFTCollection, error) {
+func (q *NFTCollectionMutator) InsertNFTCollection(blockchain string, network string, contractName string, contractAddress string) (*eth.NFTCollection, error) {
 	collection := &eth.NFTCollection{
-		Blockchain:      blockchainNetwork.Blockchain,
-		Network:         blockchainNetwork.Network,
+		Blockchain:      blockchain,
+		Network:         network,
 		ContractAddress: contractAddress,
 		Name:            contractName,
 		FromBlockHeight: *bunbig.FromInt64(0),
 	}
 
 	err := q.Session.RunInTx(q.Ctx, nil, func(ctx context.Context, tx bun.Tx) error {
+		// Query record ID if exists
+		tx.NewSelect().
+			Model(collection).
+			Where("blockchain = ? AND network = ? AND contract_address = ?", collection.Blockchain, collection.Network, collection.ContractAddress).
+			Limit(1).
+			Scan(ctx)
+
 		_, err := tx.NewInsert().
 			Model(collection).
+			On("CONFLICT (blockchain, network, contract_address) DO NOTHING").
 			Returning("*").
 			Exec(ctx)
 		return err
