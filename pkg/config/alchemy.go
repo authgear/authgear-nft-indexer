e329b4348723557f7fc59fbe9f4a9461bee9c418
Rico <hochiw@oursky.com>
2022-08-19 17:34:28 +0800

    Remove unnecessary alchemy configs
    refs #12

--- pkg/config/alchemy.go
+++ pkg/config/alchemy.go
@@ -5,18 +5,12 @@ var _ = Schema.Add("AlchemyConfig", `
 	"type": "object",
 	"additionalProperties": false,
 	"properties": {
-		"blockchain": { "type": "string" },
-		"network": { "type": "string" },
-		"endpoint": { "type": "string" },
 		"api_key": { "type": "string" }
 	},
-	"required": ["blockchain", "network", "endpoint", "api_key"]
+	"required": ["api_key"]
 }
 `)
 
 type AlchemyConfig struct {
-	Blockchain string `json:"blockchain"`
-	Network    string `json:"network"`
-	Endpoint   string `json:"endpoint"`
-	APIKey     string `json:"api_key"`
+	APIKey string `json:"api_key"`
 }
