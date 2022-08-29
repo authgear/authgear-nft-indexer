5159bbcf456ad4fb22e2767c964aa2422be33af8
Rico <hochiw@oursky.com>
2022-08-29 11:20:44 +0800

    Update nft collection table types
    refs #14

--- migrations/20220804180546-initialize_tables.sql
+++ migrations/20220804180546-initialize_tables.sql
@@ -1,6 +1,5 @@
 -- +migrate Up
 
-CREATE TYPE eth_nft_collection_type AS ENUM ('erc721');
 CREATE TABLE eth_nft_collection
 (
 	id text PRIMARY KEY,
@@ -9,8 +8,8 @@ CREATE TABLE eth_nft_collection
 	blockchain text NOT NULL,
 	network text NOT NULL,
 	from_block_height bigint NOT NULL,
-	total_supply numeric NOT NULL,
-	type eth_nft_collection_type NOT NULL,
+	total_supply bigint NOT NULL,
+	type text NOT NULL,
 	created_at    timestamp without time zone NOT NULL,
     updated_at    timestamp without time zone NOT NULL
 );
@@ -100,4 +99,3 @@ DROP TABLE eth_nft_transfer;
 DROP INDEX eth_nft_collection_unq_collection_idx;
 DROP TABLE eth_nft_collection;
 DROP FUNCTION eth_update_nft_owner();
-DROP TYPE eth_nft_collection_type;
