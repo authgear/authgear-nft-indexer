2a76da3fb94e9027bc6169cde877ad48d862ddb2
Rico <hochiw@oursky.com>
2022-10-07 11:20:43 +0800

    Create revamped tables
    refs #31

---
+++ migrations/20221007111528-initialize_tables.sql
@@ -0,0 +1,51 @@
+-- +migrate Up
+
+
+CREATE TABLE eth_nft_collection
+(
+	id text PRIMARY KEY,
+	blockchain text NOT NULL,
+	network text NOT NULL,
+	contract_address text NOT NULL,
+	name text NOT NULL,
+	total_supply bigint,
+	type text NOT NULL,
+
+	created_at    timestamp without time zone NOT NULL,
+    updated_at    timestamp without time zone NOT NULL
+);
+
+CREATE UNIQUE INDEX eth_nft_collection_unq_collection_idx ON eth_nft_collection (blockchain, network, contract_address);
+
+CREATE TABLE eth_nft_ownership
+(
+	blockchain text NOT NULL,
+	network text NOT NULL,
+	contract_address text NOT NULL,
+	token_id text NOT NULL,
+	balance text NOT NULL,
+	block_number bigint NOT NULL,
+	owner_address text NOT NULL,
+	txn_hash text NOT NULL,
+	block_timestamp timestamp without time zone NOT NULL,
+	created_at    timestamp without time zone NOT NULL,
+	updated_at    timestamp without time zone NOT NULL
+);
+
+CREATE UNIQUE INDEX eth_nft_ownership_unq_owner_idx ON eth_nft_ownership (blockchain, network, contract_address, token_id);
+CREATE INDEX eth_nft_ownership_owned_idx ON eth_nft_ownership (blockchain, network, owner_address);
+
+CREATE TABLE nft_owner
+(
+	blockchain text NOT NULL,
+	network text NOT NULL,
+	address text NOT NULL,
+	last_synced_at timestamp without time zone NOT NULL
+);
+
+CREATE UNIQUE INDEX nft_owner_unq_owner_idx ON nft_owner (blockchain, network, address);
+
+-- +migrate Down
+DROP TABLE nft_owner;
+DROP TABLE eth_nft_ownership;
+DROP TABLE eth_nft_collection;
