62da9d2aa503996c37d731ac1bf8525eebaa7c9f
Louis Chan <louischan@oursky.com>
2025-01-20 12:39:05 +0800

    Set up flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A basic flake with a shell";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # This is go 1.23.4 unless you update flake.lock
+            pkgs.go
+          ];
+        };
+      }
+    );
+}
