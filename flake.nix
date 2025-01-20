c53f3118328e1f466656c4ac78988bb00d684d56
Louis Chan <louischan@oursky.com>
2025-01-20 11:09:48 +0800

    Update go to 1.23.4

--- flake.nix
+++ flake.nix
@@ -25,6 +25,43 @@
           packages = [
             # This is go 1.23.4 unless you update flake.lock
             pkgs.go
+
+            (pkgs.golangci-lint.overrideAttrs (
+              prev:
+              let
+                version = "1.63.4";
+              in
+              {
+                inherit version;
+                src = pkgs.fetchFromGitHub {
+                  owner = "golangci";
+                  repo = "golangci-lint";
+                  rev = "v${version}";
+                  hash = "sha256-7nIo6Nuz8KLuQlT7btjnTRFpOl+KVd30v973HRKzh08=";
+                };
+                vendorHash = "sha256-atr4HMxoPEfGeaNlHqwTEAcvgbSyzgCe262VUg3J86c=";
+                # We do not actually override anything here,
+                # but if we do not repeat this, ldflags refers to the original version.
+                ldflags = [
+                  "-s"
+                  "-X main.version=${version}"
+                  "-X main.commit=v${version}"
+                  "-X main.date=19700101-00:00:00"
+                ];
+              }
+            ))
+
+            (pkgs.buildGoModule {
+              name = "wire";
+              src = pkgs.fetchFromGitHub {
+                owner = "google";
+                repo = "wire";
+                rev = "v0.5.0";
+                sha256 = "sha256-9xjymiyPFMKbysgZULmcBEMI26naUrLMgTA+d7Q+DA0=";
+              };
+              vendorHash = "sha256-ZFUX4LgPte6oAf94D82Man/P9VMpx+CDNCTMBwiy9Fc=";
+              subPackages = [ "cmd/wire" ];
+            })
           ];
         };
       }
